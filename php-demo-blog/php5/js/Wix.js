32a745ae0eb501c2ea2e78d72ad23000d378ee54
amirm <amirm@wix.com>
2012-05-14 16:08:34 +0300

    adding intent=tpa and componentType to postMessage calls

--- php-demo-blog/php5/js/Wix.js
+++ php-demo-blog/php5/js/Wix.js
@@ -10,12 +10,33 @@ var Wix = (function () {
             var target = parent.postMessage ? parent : (parent.document.postMessage ? parent.document : undefined);
             if (target && typeof target != "undefined") {
                 target.postMessage(JSON.stringify({
+					intent: "TPA",
+					compId: compId,
                     type:type,
                     data:data
                 }), "*");
             }
-        }
+        },
+		
+		getQueryParameter: function ( parameterName ) {
+		  var queryString = location.search.substring(1);
+		  var parameterName = parameterName + "=";
+		  if ( queryString.length > 0 ) {
+			begin = queryString.indexOf ( parameterName );
+			if ( begin != -1 ) {
+			  begin += parameterName.length;
+			  end = queryString.indexOf ( "&" , begin );
+				if ( end == -1 ) {
+				end = queryString.length
+			  }
+			  return unescape ( queryString.substring ( begin, end ) );
+			}
+		  }
+		  return "null";
+		}
     };
+	
+	var compId = _w.getQueryParameter("compId");
 
     return {
         reportHeightChange:function (height) {
